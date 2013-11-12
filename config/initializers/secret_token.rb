1a2e51336876740350f78769b13009f09f4ed253
Aaron Groch <ar.groch@gmail.com>
2013-11-12 08:21:20 -0500

    Initial creation. Bam.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nile::Application.config.secret_key_base = '84571b521fdd7324a3d8d933794a951098be21ea74381f0ff2ec28eaabb20fe6dcedb62d902b365a051b122e0b7729856e354755306c13ec7da9bb275e969e8d'
